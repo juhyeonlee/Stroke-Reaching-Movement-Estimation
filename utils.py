f40a09f8d09f8acf55b30a11d88d539d22682317
Juhyeon Lee <jhlee899@gmail.com>
2019-10-31 00:51:30 -0400

    add misc. functions

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 from sklearn.metrics import confusion_matrix, f1_score
+from scipy.integrate import trapz
+from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 import itertools
 from sklearn.svm import SVC
@@ -25,7 +27,6 @@ def plot_confusion_matrix(ax, y_true, y_pred, classes,
     Normalization can be applied by setting `normalize=True`.
     """
     cm = confusion_matrix(y_true, y_pred)
-
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis] * 100
 
@@ -35,11 +36,12 @@ def plot_confusion_matrix(ax, y_true, y_pred, classes,
     plt.colorbar(im)
     tick_marks = np.arange(len(classes))
     ax.set_xticks(tick_marks)
+    plt.ylim(-0.5, 2.5)
     ax.set_yticks(tick_marks)
     ax.set_xticklabels(classes, rotation=45)
     ax.set_yticklabels(classes)
 
-    fmt = '3.0f' if normalize else 'd'
+    fmt = '0.2f' if normalize else 'd'
     thresh = cm.max() / 2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         ax.text(j, i, format(cm[i, j], fmt) + ('%' if normalize else ''),
@@ -81,6 +83,86 @@ class ExhaustiveForwardSelect:
 
 
 def efs_score(X, y):
+    # model = RandomForestClassifier(n_estimators=100, random_state=0, class_weight='balanced').fit(X, y)
     model = SVC(random_state=0, class_weight='balanced', gamma='scale').fit(X, y)
     y_hat = model.predict(X)
     return f1_score(y, y_hat, average='macro')
+
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+def extract_primary_mag(vmag, threshold=np.sqrt(2) * 0.01, min_len=10):
+
+    # Threshold vmag then find zeros, add start and end as potential bounds
+    tvmag = vmag.copy()
+    tvmag[tvmag < threshold] = 0  # Apply threshold
+    zidx = np.where(tvmag == 0)  # Find indices of zeros
+    zidx = np.insert(zidx, 0, 0)  # Add start index
+    zidx = np.append(zidx, len(vmag) - 1)  # Add end index
+    zidx = np.unique(zidx)  # Remove duplicates (at start or end)
+
+    # Extract candidate bounds from segments longer than the minimum length
+    bounds = []  # Index bounds of candidate primary movements
+    for si, sf in pairwise(zidx):
+        if (sf - si) >= min_len:
+            bounds.append((si, sf + 1))
+    if len(bounds) == 0:
+        return [(0, len(vmag))]
+
+    # Merge adjacent bounds where the gap is smaller then the minimum length
+    merged_bounds = []
+    start = None
+    end = None
+    for i in range(len(bounds) - 1):
+        si1, sf1 = bounds[i]
+        si2, sf2 = bounds[i + 1]
+        if start is None:
+            if (si2 - sf1) <= min_len:
+                start = si1
+                end = sf2
+            else:
+                merged_bounds.append(bounds[i])
+        else:
+            if (si2 - sf1) <= min_len:
+                end = sf2
+            else:
+                merged_bounds.append((start, end))
+                start = None
+                end = None
+    if start is None:
+        merged_bounds.append(bounds[-1])
+    else:
+        merged_bounds.append((start, end))
+
+    # Pick the highest distance segment as the primary movement
+    distances = []
+    for si, sf in merged_bounds:
+        distances.append(trapz(vmag[si:sf], dx=0.01))
+    si, sf = merged_bounds[np.argmax(distances)]
+    return [(si, sf)]
+
+
+def extract_full_mvmt(vel):
+    return [(0, vel.shape[0])]
+
+
+def detect_zero_crossings(v):
+    zc = [0, v.shape[0]-1]
+    for i in range(v.shape[0]-1):
+        if (v[i] < 0 and v[i+1] >= 0) or (v[i] > 0 and v[i+1] <= 0):
+            zc.append(i+1)
+    zc = np.unique(zc)
+    sI, sF = zc[:-1], zc[1:]
+    return sI, sF
+
+
+def extract_first_me(vmag):
+    si, sf = detect_zero_crossings(vmag)
+    if len(si) > 1:
+        print('more than one me')
+    return [(si[0], sf[0])]
