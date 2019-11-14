b1168c7e6bede3d6dd355bf6f9550e636e132481
Juhyeon Lee <jhlee899@gmail.com>
2019-11-13 21:23:54 -0500

    nested LOSO CV

--- classify.py
+++ classify.py
@@ -4,20 +4,19 @@ import pickle
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler, RobustScaler
 from sklearn.svm import SVC
-from scipy.stats import entropy
 from sklearn.model_selection import GridSearchCV, cross_val_score, KFold
-from scipy.signal import butter, filtfilt, resample, find_peaks, peak_prominences
-from scipy.stats import pearsonr, spearmanr, linregress
+from scipy.signal import resample
 from sklearn.feature_selection import SelectKBest, f_classif, f_regression, RFE
-import seaborn as sns
-from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, classification_report
+from sklearn.metrics import accuracy_score, f1_score, classification_report, davies_bouldin_score, roc_curve
 from sklearn.ensemble import RandomForestClassifier
+from ReliefF import ReliefF
+# from skfeature.function.statistical_based import CFS
+from skfeature.function.information_theoretical_based import FCBF
+import seaborn as sns
 
-from utils import magnitude, rms, ExhaustiveForwardSelect, efs_score, plot_confusion_matrix, \
-    extract_primary_mag, extract_full_mvmt, magnitude_xy
 from extract_features import extract_features
 from data_struct import CompData
-from ReliefF import ReliefF
+from utils import ExhaustiveForwardSelect, efs_score, plot_confusion_matrix, magnitude_xy, CFS
 
 
 num_subjects = 20
@@ -96,36 +95,105 @@ all_sub = np.asarray(all_sub)
 print(len(all_sub), len(features))
 predicted = np.zeros(compensation_labels.shape)
 
-p_grid ={'C': np.logspace(-4, 4, 10), 'gamma': [0.01, 0.1, 1.0]}
+# p_grid ={'C': [0.001, 0.01, 0.1, 1, 10], 'gamma': [0.001, 0.01, 0.1, 1]}
+p_grid ={'C': [0.1, 1, 10], 'gamma': [0.0001, 0.001, 0.01]}
+r_grid = [int(x) for x in np.linspace(start=200, stop=2000, num=10)]
+selnum_f_grid = np.arange(1, len(feature_names))
+num_comb = len(p_grid['C']) * len(p_grid['gamma']) * len(selnum_f_grid)
+print('total combination grid search', num_comb)
+# selnum_f = 17
 
 for s in subject_id:
     train_feats = features[all_sub != s, :]
     train_labels = compensation_labels[all_sub != s]
     test_feats = features[all_sub == s, :]
+    train_sub = all_sub[all_sub != s]
 
-    ss = RobustScaler().fit(train_feats)
-    train_feats = ss.transform(train_feats)
-    test_feats = ss.transform(test_feats)
+    scaler = RobustScaler().fit(train_feats)
+    train_feats = scaler.transform(train_feats)
+    test_feats = scaler.transform(test_feats)
+
+    inner_subject_id = subject_id.copy()
+    inner_subject_id = np.delete(inner_subject_id, np.argwhere(inner_subject_id == s))
+
+    val_scores = np.zeros(num_comb)
+
+    j = 0
+    for n_keep in selnum_f_grid:
+        for cval in p_grid['C']:
+            for gamval in p_grid['gamma']:
+                predicted_val = np.zeros(len(train_labels))
+                for i_s in inner_subject_id:
+                    inner_train_feats = features[np.logical_and(all_sub != s, all_sub != i_s), :]
+                    # inner_train_feats = train_feats[train_sub != i_s, :]
+                    inner_train_labels = compensation_labels[np.logical_and(all_sub != s, all_sub != i_s)]
+                    val_feats = features[all_sub == i_s, :]
+                    # val_feats = train_feats[train_sub == i_s, :]
+                    val_labels = compensation_labels[all_sub == i_s]
+
+                    scaler_inner = RobustScaler().fit(inner_train_feats)
+                    inner_train_feats = scaler_inner.transform(inner_train_feats)
+                    val_feats = scaler_inner.transform(val_feats)
+
+                    # # ReliefF
+                    fsel = ReliefF(n_neighbors=10, n_features_to_keep=n_keep)
+                    inner_train_feats = fsel.fit_transform(inner_train_feats, inner_train_labels)
+                    val_feats = fsel.transform(val_feats)
+
+                    # CFS
+                    # fs = CFS().fit(inner_train_feats, inner_train_labels, n_keep)
+                    # inner_train_feats = fs.transform(inner_train_feats)
+                    # f_name = fs.get_features_name(feature_names)
+                    # print(f_name)
+                    # val_feats = fs.transform(val_feats)
+                    # # print(feature_names[cfs_idx])
+                    # # inner_train_feats = inner_train_feats[:, cfs_idx]
+                    # # val_feats = val_feats[:, cfs_idx]
+
+                    # # FCBF
+                    # cfs_idx, dfd = FCBF.fcbf(inner_train_feats, inner_train_labels)
+                    # print(feature_names[cfs_idx])
+                    # print(dfd)
+                    # inner_train_feats = inner_train_feats[:, cfs_idx]
+                    # val_feats = val_feats[:, cfs_idx]
+
+                    val_model = SVC(C=cval, gamma=gamval, kernel='rbf', random_state=0, class_weight='balanced')
+                    val_model.fit(inner_train_feats, inner_train_labels)
+                    predicted_val[train_sub == i_s] = val_model.predict(val_feats)
+                val_scores[j] = f1_score(train_labels, predicted_val)
+                j += 1
+
+    best_gamma = p_grid['gamma'][int(np.argmax(val_scores) % len(p_grid['gamma']))]
+    best_C = p_grid['C'][int(np.argmax(val_scores) // len(p_grid['gamma']) % len(p_grid['C']))]
+    best_n_keep = selnum_f_grid[int(np.argmax(val_scores) // (len(p_grid['gamma'])*len(p_grid['C'])))]
+    print('r', np.max(val_scores), np.argmax(val_scores), best_C, best_gamma, best_n_keep)
 
     # fsel = SelectKBest(f_classif, k=).fit(train_feats, train_labels)
     #     fsel = RFE(estimator=RandomForestClassifier(n_estimators=10, random_state=0, class_weight='balanced'),  n_features_to_select=15).fit(train_feats, train_labels)
     # fsel = ExhaustiveForwardSelect(score_func=efs_score).fit(train_feats, train_labels)
-    fsel = ReliefF(n_neighbors=10, n_features_to_keep=20)
     # print('Subject {}: '.format(s) + ', '.join(feature_names[fsel.selected_features]))
+
+    fsel = ReliefF(n_neighbors=10, n_features_to_keep=best_n_keep)
     train_feats = fsel.fit_transform(train_feats, train_labels)
+    print(feature_names[fsel.top_features])
     test_feats = fsel.transform(test_feats)
-    print(train_feats.shape, test_feats.shape)
+
+    # cfs_idx = CFS.cfs(train_feats, train_labels)
+    # fs = CFS().fit(train_feats, train_labels, best_n_keep)
+    # train_feats = fs.transform(train_feats)
+    # f_name = fs.get_features_name(feature_names)
+    # print(f_name)
+    # test_feats = fs.transform(test_feats)
+
+    # cfs_idx, _ = FCBF.fcbf(train_feats, train_labels)
+    # train_feats = train_feats[:, cfs_idx]
+    # test_feats = test_feats[:, cfs_idx]
+    # print(feature_names[cfs_idx])
 
     # model = RandomForestClassifier(n_estimators=100, random_state=0, class_weight='balanced').fit(train_feats, train_labels)
-    model = SVC(kernel='rbf', random_state=0, class_weight='balanced', gamma='scale')
-    # clf = GridSearchCV(estimator=model, param_grid=p_grid, iid=False, verbose=True)
-    # clf.fit(train_feats, train_labels)
-    # # print(clf.best_estimator_.C, clf.best_estimator_.gamma)
-    # #
-    # nested_score = cross_val_score(clf, X=train_feats, y=train_labels, cv=outer_cv)
-    # nested_scores[i] = nested_score.mean()
+    model = SVC(kernel='rbf', random_state=0, class_weight='balanced', C=best_C, gamma=best_gamma)
+
     model.fit(train_feats, train_labels)
-    # print(feature_names[np.flip(np.argsort(model.feature_importances_))])
     predicted[all_sub == s] = model.predict(test_feats)
 
 
@@ -135,7 +203,8 @@ fig_conf = plt.figure(figsize=[10, 10])
 ax = fig_conf.add_subplot(111)
 plot_confusion_matrix(ax, compensation_labels, predicted, ['Comps', 'Poor+Inacc', 'Normal'], normalize=True)
 fig_conf.tight_layout()
-fig_conf.savefig('confusion_matrix')
+fig_conf.savefig('confusion_matrix_2class')
+np.save('fcbf_F1', predicted)
 
 print('Accuracy: {:.1f}%'.format(accuracy_score(compensation_labels, predicted) * 100))
 print(classification_report(compensation_labels, predicted))
